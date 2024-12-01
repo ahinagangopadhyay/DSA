87b90a70943bbbea51dd5e241d1a1b10cbefb808
Ahina Gangopadhyay <159465464+ahinagangopadhyay@users.noreply.github.com>
2024-12-01 14:40:08 +0530

    Create Array by Parity.cpp

---
+++ Array by Parity.cpp
@@ -0,0 +1,26 @@
+//Sort array by Parity using two pointers
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {     
+       int i=0;
+       int j=nums.size()-1;
+       while(i<j){
+           if(nums[i]%2 == 0 & nums[j]%2 == 0){
+               i = i+1;
+           }
+           else if(nums[i]%2 == 0 & nums[j]%2 != 0){
+                i = i+1;
+                j = j-1;   
+           }
+           else if(nums[i]%2 != 0 & nums[j]%2 == 0){
+                swap(nums[i],nums[j]);
+                i = i+1;
+                j = j-1;
+           }
+           else{
+               j = j-1;
+           }
+       }
+        return nums;
+    }
+};
