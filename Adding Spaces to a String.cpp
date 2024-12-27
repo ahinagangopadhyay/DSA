190fc4f2b7c8459bf549ede65e25fbeb2a44c9b2
Ahina Gangopadhyay <159465464+ahinagangopadhyay@users.noreply.github.com>
2024-12-27 15:21:33 +0530

    Update Adding Spaces to a String.cpp

--- Adding Spaces to a String.cpp
+++ Adding Spaces to a String.cpp
@@ -4,7 +4,6 @@ public:
         string s1 = "";
         int j = 0;
         int n = s.length();
-        
         for (int i = 0; i < n; i++) {
             if (j < spaces.size() && i == spaces[j]) {
                 s1.push_back(' ');  
